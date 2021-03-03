3b51e2e2042da1256ba02936e8eacad6feae6f69
tabatha wiggers <tatisabato@hotmail.com>
2021-03-03 08:17:23 -0300

    done

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,4 +1,5 @@
 import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
@@ -7,18 +8,35 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/tatchiwiggers/cker4dw5r1pr619nvx1lsynrq'
     });
+
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl }));
 
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+
+      // Creates an HTML element for your custom marker
+      const element = document.createElement('div');
+      element.className = 'marker';
+      element.style.backgroundImage = `url('${marker.image_url}')`;
+      element.style.backgroundSize = 'contain';
+      element.style.width = '25px';
+      element.style.height = '25px';
+
+      // Pass the element as an argument to the new marker
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
         .addTo(map);
     });
 
     const bounds = new mapboxgl.LngLatBounds();
+    // stretches the bound to max longitude and latitude of markers
     markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+    // tells the map to respect the bounds!
     map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
 
   }
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'cleaning DB...'
+Flat.destroy_all
+puts 'Done cleaning DB...'
+
+
+flat = Flat.create!(name: "Bom prato", address: "R. 25 de Março, 166, Centro Histórico de São Paulo")
+puts "Flat Bom Prato created"
+puts '-----------------------------'
+
+flat = Flat.create!(name: "Popular", address: "440 Matias Aires, Sao Paulo")
+puts "Flat Popular created"
+puts '-----------------------------'
+
+
+flat = Flat.create!(name: "Comedoria Sesc Pinheiros", address: "170 Copernico, Belo Horizonte")
+puts "Restaurant Comedoria Sesc Pinheiros created"
+puts '-----------------------------'
